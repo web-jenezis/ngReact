f22e032c313f90edca18510619776c1aad655e18
dmedved <web.jenezis@gmail.com>
2015-02-06 01:54:50 +0200

    SetUp RequireJS

--- app/main.js
+++ app/main.js
@@ -1,42 +1,18 @@
-(function(ng, React){ 
-    var app = ng.module('app', []);    
-    
-    app.service('User', function() {
-        var users = [
-            {id: 1, firstName: "Denis", lastName: "Stoyanov", age:26},
-            {id: 2, firstName: "Alex", lastName: "Alexeev", age:24},
-            {id: 3, firstName: "Peter", lastName: "Petrov", age:21},
-            {id: 4, firstName: "Ivan", lastName: "Ivanov", age:20}
-        ];
-
-        return {
-            users: users
-        };
-    });
-
-    app.controller('MainCtrl', function($scope, $log, User) {
-        $scope.users = User.users;
-
-        this.getUser = function(user) {
-            $log.info('A selected user %O', user);
-        };
-    });
-
-    app.directive('userList', function() {
-        return {
-            restrict: 'AE',
-            scope: {
-                users: '=',
-                userSelected: '&'
-            },
-            link: function(scope, element, attrs) {
-                scope.$watchCollection('users', function() {
-                    React.renderComponent(UserList({scope: scope}), element[0]);
-                });
-            }
-        };
-    });
-
-    return app;
-
-})(angular, React);
+require.config({
+    paths:{
+        domReady: 'app/components/requirejs-domready/domReady',
+        jquery: '/app/components/jquery/dist/jquery',
+        bootstrap: '/app/components/bootstrap/dist/js/bootstrap',
+        angular: 'app/components/angular/angular',
+        react: 'app/components/react/react'
+    },
+    shim: {
+        angular: {
+            exports: 'angular'
+        },
+        React: {
+            exports: 'React'
+        }
+    },
+    deps: ['app/bootstrap']
+});
